4ad492a77b7bdaa340c96bc9b277e5cba2437eb0
ErmonGabriel237 <ermongabrielelateekoa@gmail.com>
2026-01-02 16:14:22 -0500

    fixed an issue with auto ads

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,7 @@ export default function App() {
 
   useEffect(() => {
     scrollToHashOnLoad(72);
-    try {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  } catch (err) {}
-  }, [],[location]);
+  }, []);
 
   return (
     <I18nProvider>
